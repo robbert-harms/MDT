db43229fff4f1c68cde7c7b3dc975542c2d01a91
Robbert Harms <robbert@xkls.nl>
2016-02-06 18:10:04 +0100

    Made the Tensor post diffusivity sorting faster

--- mdt/data/components/standard/compartment_models/Tensor.py
+++ mdt/data/components/standard/compartment_models/Tensor.py
@@ -28,39 +28,37 @@ class Tensor(CompartmentConfig):
         eigen_vectors = eigen_vectors_from_tensor(results_dict[self.name + '.theta'], results_dict[self.name + '.phi'],
                                                   results_dict[self.name + '.psi'])
 
-        eigen_values = np.concatenate([m[..., None] for m in [results_dict[self.name + '.d'],
-                                                              results_dict[self.name + '.dperp0'],
-                                                              results_dict[self.name + '.dperp1']]], axis=1)
-        ranking = np.argsort(eigen_values, axis=1)[:, ::-1]
+        eigen_values = np.squeeze(np.concatenate([m[..., None] for m in [results_dict[self.name + '.d'],
+                                                                         results_dict[self.name + '.dperp0'],
+                                                                         results_dict[self.name + '.dperp1']]], axis=1))
 
-        for voxel_ind in range(eigen_vectors.shape[0]):
-            for ind in range(eigen_values.shape[1]):
-                voxel_ranking = ranking[voxel_ind]
+        ranking = np.squeeze(np.argsort(eigen_values, axis=1)[:, ::-1])
+        voxels_listing = np.arange(ranking.shape[0])
 
-                eigen_values[voxel_ind] = [eigen_values[voxel_ind, i] for i in voxel_ranking]
-                eigen_vectors[voxel_ind] = [eigen_vectors[voxel_ind, i, :] for i in voxel_ranking]
+        eigen_values = [eigen_values[voxels_listing, ranking[:, ind]] for ind in range(ranking.shape[1])]
+        eigen_vectors = [eigen_vectors[voxels_listing, ranking[:, ind]] for ind in range(ranking.shape[1])]
 
         extra_maps = {}
         for ind in range(3):
-            extra_maps.update({self.name + '.vec' + repr(ind): eigen_vectors[:, ind]})
+            extra_maps.update({self.name + '.vec' + repr(ind): eigen_vectors[ind]})
 
             for dimension in range(3):
                 extra_maps.update({self.name + '.vec' + repr(ind) + '_' + repr(dimension):
-                                   eigen_vectors[:, ind, dimension]})
+                                   eigen_vectors[ind][:, dimension]})
 
-        extra_maps.update({self.name + '.d.sorted': eigen_values[:, 0],
-                           self.name + '.dperp0.sorted': eigen_values[:, 1],
-                           self.name + '.dperp1.sorted': eigen_values[:, 2]})
+        extra_maps.update({self.name + '.d.sorted': eigen_values[0],
+                           self.name + '.dperp0.sorted': eigen_values[1],
+                           self.name + '.dperp1.sorted': eigen_values[2]})
 
         fa, md = DTIMeasures(runtime_configuration.runtime_config['cl_environments'],
                              runtime_configuration.runtime_config['load_balancer']).\
-            concat_and_calculate(np.squeeze(eigen_values[:, 0]),
-                                 np.squeeze(eigen_values[:, 1]),
-                                 np.squeeze(eigen_values[:, 2]))
+            concat_and_calculate(np.squeeze(eigen_values[0]),
+                                 np.squeeze(eigen_values[1]),
+                                 np.squeeze(eigen_values[2]))
 
         extra_maps.update({self.name + '.FA': fa, self.name + '.MD': md})
 
-        extra_maps.update({self.name + '.AD': eigen_values[:, 0]})
-        extra_maps.update({self.name + '.RD': (eigen_values[:, 1] + eigen_values[:, 2]) / 2})
+        extra_maps.update({self.name + '.AD': eigen_values[0]})
+        extra_maps.update({self.name + '.RD': (eigen_values[1] + eigen_values[2]) / 2})
 
         return extra_maps
