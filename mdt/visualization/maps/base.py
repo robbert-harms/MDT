Fixed the rotation thing

mdt/visualization/maps/base.py
@@ -18,7 +18,7 @@ __email__ = "robbert.harms@maastrichtuniversity.nl"
 
 class MapPlotConfig(object):
 
-    def __init__(self, dimension=2, slice_index=0, volume_index=0, rotate=270, colormap='hot', maps_to_show=(),
+    def __init__(self, dimension=2, slice_index=0, volume_index=0, rotate=0, colormap='hot', maps_to_show=(),
                  font=None, grid_layout=None, colorbar_nmr_ticks=10, show_axis=True, zoom=None,
                  map_plot_options=None):
         """Container for all plot related settings.
@@ -78,6 +78,17 @@ class MapPlotConfig(object):
     @classmethod
     def from_dict(cls, config_dict):
         return cls.get_conversion_info().from_dict(config_dict)
+
+    def get_rotation(self):
+        """Get the rotation we would like to apply on the configuration.
+
+        This can differ from the instance variable rotate. The instance variable is what the user set,
+        this function returns what we make of it.
+
+        Returns:
+            int: a angle as multiple of 90, can be negative.
+        """
+        return -self.rotate - 90
 
     def to_dict(self):
         return self.get_conversion_info().to_dict(self)
@@ -256,8 +267,11 @@ class Point(object):
         return Point(*self._rotate_coordinate(self.x, self.y, rotate, shape))
 
     def _rotate_coordinate(self, x, y, rotate, shape):
+        positive_number_of_90deg_rotations = (rotate % 360) // 90
+
         rx, ry = x, y
-        for rotation in range(1, rotate // 90 + 1):
+        for rotation in range(1, positive_number_of_90deg_rotations + 1):
+            # apply rotation and translate on the x component
             current_max_x, current_max_y = np.roll(shape, rotation)
             rx, ry = current_max_y - ry, rx
         return rx, ry

mdt/visualization/maps/matplotlib_renderer.py
@@ -90,7 +90,7 @@ class AxisData(object):
         Returns
             x, y, z, v : Index coordinates of the map associated with the image.
         """
-        rotate = self._plot_config.rotate
+        rotate = self._plot_config.get_rotation()
         if not rotate:
             rotate = 0
 
@@ -155,8 +155,8 @@ class Renderer(object):
         axis.axis('on' if self._plot_config.show_axis else 'off')
 
         data = self._get_image(map_name)
-        if self._plot_config.rotate:
-            data = np.rot90(data, self._plot_config.rotate // 90)
+        if self._plot_config.get_rotation():
+            data = np.rot90(data, self._plot_config.get_rotation() // 90)
         data = self._plot_config.zoom.apply(data)
         data = self._get_map_attr(map_name, 'clipping', Clipping()).apply(data)
 