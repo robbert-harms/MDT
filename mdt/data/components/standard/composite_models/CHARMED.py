579194649cbe186ed98a42002bb2ff1df062cd4e
Robbert Harms <robbert@xkls.nl>
2017-03-09 12:01:36 +0100

    Reverted some of the diffusivity limits in CHARMED

--- mdt/data/components/standard/composite_models/CHARMED.py
+++ mdt/data/components/standard/composite_models/CHARMED.py
@@ -17,8 +17,15 @@ class CHARMED_r1(DMRICompositeModelConfig):
                )
     '''
 
-    lower_bounds = {'CHARMEDRestricted0.d': 0.3e-9}
-    upper_bounds = {'CHARMEDRestricted0.d': 3e-9}
+    lower_bounds = {'Tensor.d': 0.3e-9,
+                    'Tensor.dperp0': 0.3e-9,
+                    'Tensor.dperp1': 0.3e-9,
+                    'CHARMEDRestricted0.d': 0.3e-9}
+
+    upper_bounds = {'Tensor.d': 5e-9,
+                    'Tensor.dperp0': 5e-9,
+                    'Tensor.dperp1': 5e-9,
+                    'CHARMEDRestricted0.d': 3e-9}
 
     inits = {'Tensor.d': 1.2e-9,
              'Tensor.dperp0': 0.5e-9,
@@ -41,10 +48,16 @@ class CHARMED_r2(DMRICompositeModelConfig):
                (Weight(w_res1) * CHARMEDRestricted(CHARMEDRestricted1)) )
     '''
 
-    lower_bounds = {'CHARMEDRestricted0.d': 0.3e-9,
+    lower_bounds = {'Tensor.d': 0.3e-9,
+                    'Tensor.dperp0': 0.3e-9,
+                    'Tensor.dperp1': 0.3e-9,
+                    'CHARMEDRestricted0.d': 0.3e-9,
                     'CHARMEDRestricted1.d': 0.3e-9}
 
-    upper_bounds = {'CHARMEDRestricted0.d': 3e-9,
+    upper_bounds = {'Tensor.d': 5e-9,
+                    'Tensor.dperp0': 5e-9,
+                    'Tensor.dperp1': 5e-9,
+                    'CHARMEDRestricted0.d': 3e-9,
                     'CHARMEDRestricted1.d': 3e-9}
 
     inits = {'Tensor.d': 1.2e-9,
@@ -72,11 +85,17 @@ class CHARMED_r3(DMRICompositeModelConfig):
                (Weight(w_res2) * CHARMEDRestricted(CHARMEDRestricted2)) )
     '''
 
-    lower_bounds = {'CHARMEDRestricted0.d': 0.3e-9,
+    lower_bounds = {'Tensor.d': 0.3e-9,
+                    'Tensor.dperp0': 0.3e-9,
+                    'Tensor.dperp1': 0.3e-9,
+                    'CHARMEDRestricted0.d': 0.3e-9,
                     'CHARMEDRestricted1.d': 0.3e-9,
                     'CHARMEDRestricted2.d': 0.3e-9}
 
-    upper_bounds = {'CHARMEDRestricted0.d': 3e-9,
+    upper_bounds = {'Tensor.d': 5e-9,
+                    'Tensor.dperp0': 5e-9,
+                    'Tensor.dperp1': 5e-9,
+                    'CHARMEDRestricted0.d': 3e-9,
                     'CHARMEDRestricted1.d': 3e-9,
                     'CHARMEDRestricted2.d': 3e-9}
 
