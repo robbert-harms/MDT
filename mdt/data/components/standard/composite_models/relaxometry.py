b6ffe6e5aecec9bfa13fa1f94cd3a2d312decd17
Robbert Harms <robbert@xkls.nl>
2018-01-18 14:25:51 +0100

    Adds relaxometry models

--- mdt/data/components/standard/composite_models/relaxometry.py
+++ mdt/data/components/standard/composite_models/relaxometry.py
@@ -13,3 +13,19 @@ class S0_T1_GRE(DMRICompositeModelTemplate):
     model_expression = 'S0 * ExpT1DecIR'
     inits = {'ExpT1DecIR.T1': 3.0}
     upper_bounds = {'ExpT1DecIR.T1': 6.0}
+
+
+class S0T2_exvivo(DMRICompositeModelTemplate):
+
+    name = 'S0-T2-ExVivo'
+    description = 'Models the unweighted signal (aka. b0) with an extra T2.'
+    model_expression = 'S0 * ExpT2Dec'
+    upper_bounds = {'ExpT2Dec.T2': 0.5}
+
+
+class S0T2(DMRICompositeModelTemplate):
+
+    name = 'S0-T2'
+    description = 'Models the unweighted signal (aka. b0) with an extra T2.'
+    model_expression = 'S0 * ExpT2Dec'
+    upper_bounds = {'ExpT2Dec.T2': 2}
