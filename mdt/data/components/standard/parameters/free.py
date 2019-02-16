770c547b96886e5511e82e5539f5d11d66659df2
Robbert Harms <robbert@xkls.nl>
2019-02-16 10:29:19 +0100

    Changed parameter transformation of the weights from CosSqrClamp to ScaleTransform, and let the box constraints enforce the boundary.

--- mdt/data/components/standard/parameters/free.py
+++ mdt/data/components/standard/parameters/free.py
@@ -28,7 +28,7 @@ class w(FreeParameterTemplate):
     init_value = 0.5
     lower_bound = 0
     upper_bound = 1
-    parameter_transform = 'CosSqrClamp'
+    parameter_transform = ScaleTransform(10)
     sampling_proposal_std = 0.01
     sampling_prior = UniformWithinBoundsPrior()
     numdiff_info = {'scale_factor': 10}
